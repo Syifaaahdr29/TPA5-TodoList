7ece11ce4f62f5c649577b071be9f5caa7e71e28
Syifaaahdr29 <syifaaahdr29@gmail.com>
2023-05-25 01:32:25 +0700

    make store, reducers js, and making todos add func

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Todos />
+    </div>
+  );
+}
+
+export default App
